285c5b2cce6e1dd94a7ca436136cb679f723152b
carsten <cr@tagion.org>
2020-06-30 13:00:48 +0300

    ddoc_builder.mk updated

--- ddoc_builder.mk
+++ ddoc_builder.mk
@@ -3,27 +3,25 @@ DDOCREPO?=${shell git rev-parse --show-toplevel}
 ifndef DDOCHELP
 DDOCHELP:=help-ddoc
 HELP+=$(DDOCHELP)
-help-ddoc:
-	@echo "make ddoc      : Creates source documentation"
-	@echo
-
 else
-CADRYDOOT:=$(DDOCREPO)/candydoc/
+CADRYROOT:=$(DDOCREPO)/candydoc/
 DOCBEGIN:= $(DDOCREPO)/begin.html
 DOCEND:= $(DDOCREPO)/end.html
 DDOCSCRIPT:=$(DDOCREPO)/scripts/ddocmodule.pl
 DDOCINDEX?=$(DDOCROOT)/index.html
 DDOCWORKDIR?=/tmp/$(GIT_HASH)/
+DDOCMODULES?=modules.ddoc
 
-test:
-	git rev-parse HEAD
+help-ddoc:
+	@echo "make ddoc      : Creates source documentation"
+	@echo
 
 ddoc: $(DDOCINDEX)
 
 $(DDOCINDEX): $(DDOCWORKDIR) $(DDOCMODULES)
 	@echo "########################################################################################"
 	@echo "## Creating DDOC"
-	${PRECMD}cp -a $(CADRYDOOT) $(DDOCROOT)
+	${PRECMD}cp -a $(CADRYROOT) $(DDOCROOT)
 	$(PRECMD)$(DC) ${INCFLAGS} $(DDOCFLAGS) $(DDOCFILES) $(DFILES) $(DD)$(DDOCWORKDIR)
 	$(PRECMD)cat $(DDOCBEGIN) $(DDOCWORKDIR)/*.html $(DOCBEGIN) > $@
 
